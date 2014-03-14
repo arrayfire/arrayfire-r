f992ef310f6ccff0f980e2c003c1c9aabd3a8164
pavan <pavan@accelereyes.com>
2014-03-14 16:30:13 -0400

    indexing support added in

---
+++ R/index.R
@@ -0,0 +1,162 @@
+## indexing for afArray
+#######################
+
+getIndices <- function(...) {
+    indices <- list(...)
+    if (length(indices > 0)) {
+        stop("Indexing supported for first two dimensions only")
+    }
+    return (indices)
+}
+
+setMethod("[", signature(x="afArray", i="afArray", j="afArray"),
+          function(x, i, j, ...) {
+              indices <- getIndices(...)
+
+              if (missing(i)) {
+                  ret =.Call("af_get_sass", x@ptr, 0, j, 0, 0)
+              }
+
+              if (missing(j)) {
+                  ret =.Call("af_get_asss", x@ptr, i, 0, 0, 0)
+              }
+
+              ret = .Call("af_get_aass", x@ptr, i, j, 0, 0)
+              result = new("afArray", ptr=ret)
+              return (result)
+          })
+
+setMethod("[", signature(x="afArray", i="numeric", j="afArray"),
+          function(x, i, j, ...) {
+              indices <- getIndices(...)
+
+              if (missing(i)) {
+                  ret =.Call("af_get_sass", x@ptr, 0, j, 0, 0)
+              } else {
+                  i = as.integer(i)
+              }
+
+              if (missing(j)) {
+                  ret =.Call("af_get_nsss", x@ptr, i, 0, 0, 0)
+              }
+
+              ret = .Call("af_get_nass", x@ptr, i, j, 0, 0)
+              result = new("afArray", ptr=ret)
+              return (result)
+          })
+
+setMethod("[", signature(x="afArray", i="afArray", j="numeric"),
+          function(x, i, j, ...) {
+              indices <- getIndices(...)
+
+              if (missing(j)) {
+                  ret =.Call("af_get_asss", x@ptr, i, 0, 0, 0)
+              } else {
+                  j = as.integer(j)
+              }
+
+              if (missing(i)) {
+                  ret =.Call("af_get_snss", x@ptr, 0, j, 0, 0)
+              }
+
+              ret = .Call("af_get_anss", x@ptr, i, j, 0, 0)
+              result = new("afArray", ptr=ret)
+              return (result)
+          })
+
+setMethod("[", signature(x="afArray", i="ANY", j="ANY"),
+          function(x, i, j=1, ...) {
+              indices <- getIndices(...)
+
+              if (missing(i)) { i = 0 }
+
+              i = as.integer(i)
+              j = as.integer(j)
+
+              if (sum(i) == 0) {
+                  ret = .Call("af_get_snss", x@ptr, 0, j, 0, 0)
+              } else {
+                  ret = .Call("af_get_nnss", x@ptr, i, j, 0, 0)
+              }
+
+              result = new("afArray", ptr=ret)
+              return (result)
+          })
+
+setMethod("[<-", signature(x="afArray", i="afArray", j="afArray"),
+          function(x, i, j, ..., value) {
+              indices <- getIndices(...)
+              if (class(value) != "afArray") { stop ("value parameter must be afArray") }
+
+              if (missing(i)) {
+                  ret =.Call("af_set_sass", x@ptr, value@ptr, 0, j, 0, 0)
+              }
+
+              if (missing(j)) {
+                  ret =.Call("af_set_asss", x@ptr, value@ptr, i, 0, 0, 0)
+              }
+
+              ret = .Call("af_set_aass", x@ptr, value@ptr, i, j, 0, 0)
+              result = new("afArray", ptr=ret)
+              return (result)
+          })
+
+setMethod("[<-", signature(x="afArray", i="numeric", j="afArray"),
+          function(x, i, j, ..., value) {
+              indices <- getIndices(...)
+              if (class(value) != "afArray") { stop ("value parameter must be afArray") }
+
+              if (missing(i)) {
+                  ret =.Call("af_set_sass", x@ptr, value@ptr, 0, j, 0, 0)
+              } else {
+                  i = as.integer(i)
+              }
+
+              if (missing(j)) {
+                  ret =.Call("af_set_nsss", x@ptr, value@ptr, i, 0, 0, 0)
+              }
+
+              ret = .Call("af_set_nass", x@ptr, value@ptr, i, j, 0, 0)
+              result = new("afArray", ptr=ret)
+              return (result)
+          })
+
+setMethod("[<-", signature(x="afArray", i="afArray", j="numeric"),
+          function(x, i, j, ..., value) {
+              indices <- getIndices(...)
+              if (class(value) != "afArray") { stop ("value parameter must be afArray") }
+
+              if (missing(j)) {
+                  ret =.Call("af_set_asss", x@ptr, value@ptr, i, 0, 0, 0)
+              } else {
+                  j = as.integer(j)
+              }
+
+              if (missing(i)) {
+                  ret =.Call("af_set_snss", x@ptr, value@ptr, 0, j, 0, 0)
+              }
+
+              ret = .Call("af_set_anss", x@ptr, value@ptr, i, j, 0, 0)
+              result = new("afArray", ptr=ret)
+              return (result)
+          })
+
+setMethod("[<-", signature(x="afArray", i="ANY", j="ANY"),
+          function(x, i, j=1, ..., value) {
+              indices <- getIndices(...)
+              if (class(value) != "afArray") { stop ("value parameter must be afArray") }
+
+              if (missing(i)) { i = 0 }
+
+              i = as.integer(i)
+              j = as.integer(j)
+
+              if (sum(i) == 0) {
+                  ret = .Call("af_set_snss", x@ptr, value@ptr, 0, j, 0, 0)
+              } else {
+                  ret = .Call("af_set_nnss", x@ptr, value@ptr, i, j, 0, 0)
+              }
+
+              result = new("afArray", ptr=ret)
+              return (result)
+          })
---
+++ src/index.cpp
@@ -0,0 +1,287 @@
+#include <stdio.h>
+#include "R_wrapper.h"
+using namespace af;
+
+
+/// Specify which address-space pointer belongs
+typedef enum {
+    afDevice, ///< Device-memory pointer
+    afHost    ///< Host-memory pointer
+} af_source_t;
+
+EXTERNC af_get_nnss(SEXP x, SEXP i, SEXP j, SEXP k, SEXP l)
+{
+    // i is numeric array
+    // j is numeric array
+    // k,l are spans and can be ignored
+    int *I = IntPtr(i, 0);
+    int *J = IntPtr(j, 0);
+    array *X = getPtr(x);
+    array *res = new array();
+
+    if (length(i) == 1 && length(j) == 1) {
+        *res = (*X)(*I-1, *J-1);
+    } else if (length(i) == 1) {
+        array jj = array(length(j), J).as(f32);
+        *res = (*X)(*I-1, jj-1);
+    } else if (length(j) == 1) {
+        array ii = array(length(i), I).as(f32);
+        *res = (*X)(ii-1, *J-1);
+    }else {
+        array ii = array(length(i), I).as(f32);
+        array jj = array(length(j), J).as(f32);
+        *res = (*X)(ii-1, jj-1);
+    }
+
+    return getSEXP(res);
+}
+
+EXTERNC af_get_aass(SEXP x, SEXP i, SEXP j, SEXP k, SEXP l)
+{
+    // i is arrayfire array
+    // j is arrayfire array
+    // k,l are spans and can be ignored
+    array *I = getPtr(i);
+    array *J = getPtr(j);
+    array *X = getPtr(x);
+    array *res = new array();
+    *res = (*X)((*I).as(f32)-1, (*J).as(f32)-1);
+    return getSEXP(res);
+}
+
+EXTERNC af_get_anss(SEXP x, SEXP i, SEXP j, SEXP k, SEXP l)
+{
+    // i is arrayfire array
+    // j is numeric array
+    // k,l are spans and can be ignored
+    array *I = getPtr(i);
+    int *J = IntPtr(j, 0);
+    array *X = getPtr(x);
+    array *res = new array();
+
+    if (length(j) == 1) {
+        *res = (*X)((*I).as(f32)-1, *J-1);
+    } else {
+        array jj = array(length(j), J).as(f32);
+        *res = (*X)((*I).as(f32)-1, jj-1);
+    }
+
+    return getSEXP(res);
+}
+
+EXTERNC af_get_nass(SEXP x, SEXP i, SEXP j, SEXP k, SEXP l)
+{
+    // i is numeric array
+    // j is arrayfire array
+    // k,l are spans and can be ignored
+    int *I = IntPtr(i, 0);
+    array *J = getPtr(j);
+    array *X = getPtr(x);
+    array *res = new array();
+
+    if (length(i) == 1) {
+        *res = (*X)((*I)-1, (*J).as(f32)-1);
+    } else {
+        array ii = array(length(i), I).as(f32);
+        *res = (*X)(ii-1, (*J).as(f32)-1);
+    }
+
+    return getSEXP(res);
+}
+
+EXTERNC af_get_sass(SEXP x, SEXP i, SEXP j, SEXP k, SEXP l)
+{
+    // j is arrayfire array
+    // i, k,l are spans and can be ignored
+    array *J = getPtr(j);
+    array *X = getPtr(x);
+    array *res = new array();
+    *res = (*X)(span, (*J).as(f32)-1);
+    return getSEXP(res);
+}
+
+EXTERNC af_get_asss(SEXP x, SEXP i, SEXP j, SEXP k, SEXP l)
+{
+    // i is arrayfire array
+    // j, k,l are spans and can be ignored
+    array *I = getPtr(i);
+    array *X = getPtr(x);
+    array *res = new array();
+    *res = (*X)((*I).as(f32)-1, span);
+    return getSEXP(res);
+}
+
+
+EXTERNC af_get_snss(SEXP x, SEXP i, SEXP j, SEXP k, SEXP l)
+{
+    // j is numeric array
+    // i, k,l are spans and can be ignored
+    int *J = IntPtr(j, 0);
+    array *X = getPtr(x);
+    array *res = new array();
+
+    if (length(j) == 1) {
+        *res = (*X)(span, *J-1);
+    } else {
+        array jj = array(length(j), J).as(f32) - 1;
+        *res = (*X)(span, jj-1);
+    }
+    return getSEXP(res);
+}
+
+EXTERNC af_get_nsss(SEXP x, SEXP i, SEXP j, SEXP k, SEXP l)
+{
+    // i is numeric array
+    // j, k,l are spans and can be ignored
+    int *I = IntPtr(i,0);
+    array *X = getPtr(x);
+    array *res = new array();
+
+    if (length(i) == 1) {
+        *res = (*X)(span, *I-1);
+    } else {
+        array ii = array(length(i), I).as(f32) - 1;
+        *res = (*X)(span, ii-1);
+    }
+    return getSEXP(res);
+}
+
+EXTERNC af_set_nnss(SEXP x, SEXP v, SEXP i, SEXP j, SEXP k, SEXP l)
+{
+    // i is numeric array
+    // j is numeric array
+    // k,l are spans and can be ignored
+    int *I = IntPtr(i, 0);
+    int *J = IntPtr(j, 0);
+    array *X = getPtr(x);
+    array *res = new array();
+    *res = *X;
+
+    if (length(i) == 1 && length(j) == 1) {
+        (*res)(*I-1, *J-1) = *getPtr(v);
+    } else if (length(i) == 1) {
+        array jj = array(length(j), J).as(f32);
+        (*res)(*I-1, jj-1) = *getPtr(v);
+    } else if (length(j) == 1) {
+        array ii = array(length(i), I).as(f32);
+        (*res)(ii-1, *J-1) = *getPtr(v);
+    }else {
+        array ii = array(length(i), I).as(f32);
+        array jj = array(length(j), J).as(f32);
+        (*res)(ii-1, jj-1) = *getPtr(v);
+    }
+
+    return getSEXP(res);
+}
+
+EXTERNC af_set_aass(SEXP x, SEXP v, SEXP i, SEXP j, SEXP k, SEXP l)
+{
+    // i is arrayfire array
+    // j is arrayfire array
+    // k,l are spans and can be ignored
+    array *I = getPtr(i);
+    array *J = getPtr(j);
+    array *X = getPtr(x);
+    array *res = new array();
+    *res = *X;
+    (*res)((*I).as(f32)-1, (*J).as(f32)-1) = *getPtr(v);
+    return getSEXP(res);
+}
+
+EXTERNC af_set_anss(SEXP x, SEXP v, SEXP i, SEXP j, SEXP k, SEXP l)
+{
+    // i is arrayfire array
+    // j is numeric array
+    // k,l are spans and can be ignored
+    array *I = getPtr(i);
+    int *J = IntPtr(j, 0);
+    array *X = getPtr(x);
+    array *res = new array();
+    *res = *X;
+    if (length(j) == 1) {
+        (*res)((*I).as(f32)-1, *J-1) = *getPtr(v);
+    } else {
+        array jj = array(length(j), J).as(f32);
+        (*res)((*I).as(f32)-1, jj-1) = *getPtr(v);
+    }
+    return getSEXP(res);
+}
+
+EXTERNC af_set_nass(SEXP x, SEXP v, SEXP i, SEXP j, SEXP k, SEXP l)
+{
+    // i is numeric array
+    // j is arrayfire array
+    // k,l are spans and can be ignored
+    int *I = IntPtr(i, 0);
+    array *J = getPtr(j);
+    array *X = getPtr(x);
+    array *res = new array();
+    *res = *X;
+    if (length(i) == 1) {
+        (*res)((*I)-1, (*J).as(f32)-1) = *getPtr(v);
+    } else {
+        array ii = array(length(i), I).as(f32);
+        (*res)(ii-1, (*J).as(f32)-1) = *getPtr(v);
+    }
+
+    return getSEXP(res);
+}
+
+EXTERNC af_set_sass(SEXP x, SEXP v, SEXP i, SEXP j, SEXP k, SEXP l)
+{
+    // j is arrayfire array
+    // i, k,l are spans and can be ignored
+    array *J = getPtr(j);
+    array *X = getPtr(x);
+    array *res = new array();
+    *res = *X;
+    (*res)(span, (*J).as(f32)-1) = *getPtr(v);
+    return getSEXP(res);
+}
+
+EXTERNC af_set_asss(SEXP x, SEXP v, SEXP i, SEXP j, SEXP k, SEXP l)
+{
+    // i is arrayfire array
+    // j, k,l are spans and can be ignored
+    array *I = getPtr(i);
+    array *X = getPtr(x);
+    array *res = new array();
+    *res = *X;
+    (*res)((*I).as(f32)-1, span) = *getPtr(v);
+    return getSEXP(res);
+}
+
+
+EXTERNC af_set_snss(SEXP x, SEXP v, SEXP i, SEXP j, SEXP k, SEXP l)
+{
+    // j is numeric array
+    // i, k,l are spans and can be ignored
+    int *J = IntPtr(j, 0);
+    array *X = getPtr(x);
+    array *res = new array();
+    *res = *X;
+    if (length(j) == 1) {
+        (*res)(span, *J-1) = *getPtr(v);
+    } else {
+        array jj = array(length(j), J).as(f32) - 1;
+        (*res)(span, jj-1) = *getPtr(v);
+    }
+    return getSEXP(res);
+}
+
+EXTERNC af_set_nsss(SEXP x, SEXP v, SEXP i, SEXP j, SEXP k, SEXP l)
+{
+    // i is numeric array
+    // j, k,l are spans and can be ignored
+    int *I = IntPtr(i,0);
+    array *X = getPtr(x);
+    array *res = new array();
+    *res = *X;
+    if (length(i) == 1) {
+        (*res)(span, *I-1) = *getPtr(v);
+    } else {
+        array ii = array(length(i), I).as(f32) - 1;
+        (*res)(span, ii-1) = *getPtr(v);
+    }
+    return getSEXP(res);
+}
