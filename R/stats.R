cb2d35a4e4539fad61b77bba4f52312493f6ffea
pavan <pavan@accelereyes.com>
2014-03-14 10:02:59 -0400

    Dimensional reductions have been added

---
+++ R/stats.R
@@ -0,0 +1,128 @@
+## Reduce entire array
+########################
+
+setMethod("sum", signature(x="afArray", na.rm="logical"),
+          function(x, na.rm=0) {
+              ret = .Call("af_sum", x@ptr, -1)
+              result <- afArrayWrap(ret)
+              return (result)
+          })
+
+setMethod("any", signature(x="afArray", na.rm="logical"),
+          function(x, na.rm=0) {
+              ret = .Call("af_anytrue", x@ptr, -1)
+              result <- afArrayWrap(ret)
+              return (result)
+          })
+
+setMethod("all", signature(x="afArray", na.rm="logical"),
+          function(x, na.rm=0) {
+              ret = .Call("af_alltrue", x@ptr, -1)
+              result <- afArrayWrap(ret)
+              return (result)
+          })
+
+setMethod("min", signature(x="afArray", na.rm="logical"),
+          function(x, na.rm=0) {
+              ret = .Call("af_min", x@ptr, -1)
+              result <- afArrayWrap(ret)
+              return (result)
+          })
+
+setMethod("max", signature(x="afArray", na.rm="logical"),
+          function(x, na.rm=0) {
+              ret = .Call("af_max", x@ptr, -1)
+              result <- afArrayWrap(ret)
+              return (result)
+          })
+
+setMethod("median", signature(x="afArray", na.rm="logical"),
+          function(x, na.rm=0) {
+              ret = .Call("af_median", x@ptr, -1)
+              result <- afArrayWrap(ret)
+              return (result)
+          })
+
+setMethod("sd", signature(x="afArray", na.rm="logical"),
+          function(x, na.rm=0) {
+              ret = .Call("af_stdev", x@ptr, -1)
+              result <- afArrayWrap(ret)
+              return (result)
+          })
+
+setGeneric("mean", function(x) standardGeneric("mean"))
+
+setMethod("mean", signature(x="afArray"),
+          function(x) {
+              ret = .Call("af_mean", x@ptr, -1)
+              result <- afArrayWrap(ret)
+              return (result)
+          })
+
+## Reduce along dimension
+#########################
+setGeneric("dimSum", function(x, dim) standardGeneric("dimSum"))
+setGeneric("dimMean", function(x, dim) standardGeneric("dimMean"))
+setGeneric("dimMin", function(x, dim) standardGeneric("dimMin"))
+setGeneric("dimMax", function(x, dim) standardGeneric("dimMax"))
+setGeneric("dimAny", function(x, dim) standardGeneric("dimAny"))
+setGeneric("dimAll", function(x, dim) standardGeneric("dimAll"))
+setGeneric("dimSd", function(x, dim) standardGeneric("dimSd"))
+setGeneric("dimMedian", function(x, dim) standardGeneric("dimMedian"))
+
+
+setMethod("dimSum", signature(x="afArray", dim="numeric"),
+          function(x, dim=1) {
+              ret = .Call("af_sum", x@ptr, dim-1)
+              result <- afArrayWrap(ret)
+              return (result)
+          })
+
+setMethod("dimMean", signature(x="afArray", dim="numeric"),
+          function(x, dim=1) {
+              ret = .Call("af_mean", x@ptr, dim-1)
+              result <- afArrayWrap(ret)
+              return (result)
+          })
+
+setMethod("dimMin", signature(x="afArray", dim="numeric"),
+          function(x, dim=1) {
+              ret = .Call("af_min", x@ptr, dim-1)
+              result <- afArrayWrap(ret)
+              return (result)
+          })
+
+setMethod("dimMax", signature(x="afArray", dim="numeric"),
+          function(x, dim=1) {
+              ret = .Call("af_max", x@ptr, dim-1)
+              result <- afArrayWrap(ret)
+              return (result)
+          })
+
+setMethod("dimAny", signature(x="afArray", dim="numeric"),
+          function(x, dim=1) {
+              ret = .Call("af_any", x@ptr, dim-1)
+              result <- afArrayWrap(ret)
+              return (result)
+          })
+
+setMethod("dimAll", signature(x="afArray", dim="numeric"),
+          function(x, dim=1) {
+              ret = .Call("af_all", x@ptr, dim-1)
+              result <- afArrayWrap(ret)
+              return (result)
+          })
+
+setMethod("dimSd", signature(x="afArray", dim="numeric"),
+          function(x, dim=1) {
+              ret = .Call("af_sd", x@ptr, dim-1)
+              result <- afArrayWrap(ret)
+              return (result)
+          })
+
+setMethod("dimMedian", signature(x="afArray", dim="numeric"),
+          function(x, dim=1) {
+              ret = .Call("af_median", x@ptr, dim-1)
+              result <- afArrayWrap(ret)
+              return (result)
+          })
